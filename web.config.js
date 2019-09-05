e0db52aa497f2428075d741e71a2cf8f6e5539c9
chige007 <461841500@qq.com>
2019-09-05 16:17:45 +0800

    修改ip端口

--- web.config.js
+++ web.config.js
@@ -6,14 +6,14 @@ const config = {
 	proxyProtocol: 'http',
 	// proxyIp: '0.0.0.0',
 	// proxyPort: '8090',
-	proxyIp: '202.104.31.181',
-	proxyPort: '13808',
+	prodIp: '0.0.0.0',
+	proxyPort: '3000',
 	proxyAPIIdentity: 'api',
 
 	// 生成环境请求地址
 	prodProtocol: 'http',
-	prodIp: '202.104.31.181',
-	prodPort: '13808',
+	prodIp: '0.0.0.0',
+	prodPort: '3000',
 	prodAPIIdentity: 'api',
 
 	dry: false
